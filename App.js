97c112dcc44b4956dfcf402e5774d95d99b41c48
InovaCodeGithub <inovagithub@gmail.com>
2021-03-01 16:10:48 -0300

    Concluida logica de front-end para enviar usuario ao Whatsapp do restaurante

--- App.js
+++ App.js
@@ -1,14 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, Button, Linking } from 'react-native'
+import { BarCodeScanner } from 'expo-barcode-scanner'
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null)
+  const [scanned, setScanned] = useState(false)
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    ;(async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync()
+      setHasPermission(status === 'granted')
+    })()
+  }, [])
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true)
+    setMessage(data)
+  }
+
+  const goToWhatsapp = () => {
+    console.log(message)
+    Linking.openURL(`${message}`)
+    setScanned(false)
+  }
+
+  if (hasPermission === null) {
+    return <Text>Perdindo por permissao para utilizar a camera</Text>
+  }
+
+  if (hasPermission === false) {
+    return <Text>Sem acesso a camera</Text>
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <BarCodeScanner onBarCodeScanned={scanned ? undefined : handleBarCodeScanned} style={StyleSheet.absoluteFillObject} />
+      {scanned && <Button title={`Ir para ${message}`} onPress={() => goToWhatsapp()} />}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +48,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
